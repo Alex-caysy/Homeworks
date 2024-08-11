267358f3ccaef898472f20f81dcc666ec7782312
Alexandr <121908870+Alex-caysy@users.noreply.github.com>
2024-08-11 15:16:10 +0300

    init commit

---
+++ IntroModule/main.py
@@ -0,0 +1,16 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(99.9 > 99.8 and 1000 != 1000.1)
+
+# 3rd program
+val1 = 2 * 2 + 2
+val2 = 2 * (2 + 2)
+print(val1)
+print(val2)
+print(val1 == val2)
+
+# 4th program
+number = float('123.456')
+print(int((number * 10) % 10))
---
+++ Module 1/strings.py
@@ -0,0 +1,6 @@
+str_ = "Это тестовая строка"
+print(str_[0])
+print(str_[-1])
+print(str_[(len(str_)//2):])
+print(str_[::-1])
+print(str_[1::2])
