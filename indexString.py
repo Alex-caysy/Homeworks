e93090bacce7c2c7f0da763bda545fd7aedfb3a3
Alexandr <121908870+Alex-caysy@users.noreply.github.com>
2024-08-19 21:38:22 +0300

    исправил кавычки

--- indexString.py
+++
@@ -1,8 +0,0 @@
-MyString = 'Это моя строка'
-MyStringNew = 'это новая строка'
-print(MyString[0])
-print(MyString[-1])
-print(MyString[2:5])
-print(MyString[::-1])
-print(len(MyString))
-print(MyStringNew + MyString)
