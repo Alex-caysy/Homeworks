57c508e44a57064d5938f638b81b910a90438396
Alexandr <121908870+Alex-caysy@users.noreply.github.com>
2024-08-30 07:16:25 +0300

    add module_3_3

--- Module 3/module_3_3.py
+++ Module 3/module_3_3.py
@@ -9,10 +9,11 @@ print_params(c=[1, 2, 3])
 # 2-ая часть.
 values_list = [3.14, 'str', [5, 6, 7]]
 values_dict = {'a': 23, 'b': 'dict', 'c': (4, 3, 2)}
-
+print('-'*20)
 print_params(*values_list)
 print_params(**values_dict)
 
 # 3-тья часть.
 values_list_2 =[54.32, 'Строка' ]
+print('-'*20)
 print(*values_list_2, 42)
