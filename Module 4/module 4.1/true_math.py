f0d9a968038b7adc151a8fe15bfa1663c20299d1
Alexandr <121908870+Alex-caysy@users.noreply.github.com>
2024-09-05 18:58:52 +0300

    add module_4_1.py, fake_math.py, true_math.py

---
+++ Module 4/module 4.1/true_math.py
@@ -0,0 +1,16 @@
+import math
+
+
+def main():
+    pass
+
+
+def divide(first, second):
+    if second == 0:
+        return math.inf
+    else:
+        return first / second
+
+
+if __name__ == '__main__':
+    main()
