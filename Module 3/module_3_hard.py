10ba96f56adc333787eaff02fe79bd82092785d1
Alexandr <121908870+Alex-caysy@users.noreply.github.com>
2024-09-05 17:17:34 +0300

    module_3_hard deleted prints

--- Module 3/module_3_hard.py
+++ Module 3/module_3_hard.py
@@ -10,15 +10,12 @@ data_structure = [
 def calculate_structure_sum(*args):
     count = 0
     args = args[0]
-    print(type(args), args)
 
     if isinstance(args, int):
-        print(f'this is int: {args}\n')
         count += args
     elif isinstance(args, str):
         count += len(args)
     elif isinstance(args, dict):
-        print(list(args.items()))
         return calculate_structure_sum(list(args.items()))
     elif isinstance(args, set):
         if len(args) == 0:
@@ -31,14 +28,10 @@ def calculate_structure_sum(*args):
             return 0
         first = calculate_structure_sum(args[0])
         if len(args) > 1:
-            print(args[0], '|', args[1:])
-            print(f'count: {count} \n')
             return first + calculate_structure_sum(args[1:])
         else:
             return first
-
     return count
-
 
 
 result = calculate_structure_sum(data_structure)
