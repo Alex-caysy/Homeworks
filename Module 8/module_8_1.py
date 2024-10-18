a816e828fc598754310a3c801e35e76f35a9d752
Alexandr <121908870+Alex-caysy@users.noreply.github.com>
2024-10-18 13:43:54 +0300

    move Module 7

---
+++ Module 8/module_8_1.py
@@ -0,0 +1,16 @@
+def add_everything_up(a, b):
+    try:
+        return a + b
+    except TypeError:
+        if isinstance(a, str):
+            b = str(b)
+        else:
+            a = str(a)
+    finally:
+        return a + b
+
+
+print(add_everything_up(10.5, 7))
+print(add_everything_up('h', 7))
+print(add_everything_up(7, 'h'))
+print(add_everything_up('hel', 'lo'))
